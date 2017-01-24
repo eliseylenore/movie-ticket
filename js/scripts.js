4897ba1d85918ec9128dd67387df4bbb222b6bb8
Caitlin Hines and Elise St Hilaire <elise@>
2017-01-24 15:42:19 -0800

    Create chosenMovie array and push values selected by users to array

--- js/scripts.js
+++ js/scripts.js
@@ -11,13 +11,17 @@ var lalaLand = new Movie("La La Land", ["10am", "12pm", "3pm", "6pm", "10pm"]);
 var lionKing = new Movie("Lion King", ["10am", "12pm", "3pm", "6pm", "10pm"]);
 var princessBride = new Movie("Princess Bride", ["10am", "12pm", "3pm", "6pm", "10pm"]);
 
-
-
+var newMovies = ["La La Land"];
+var child = "Under 10";
+var teen = "Under 18";
+var adult = "18-65"
 
 
 
 //front-end logic
 $(document).ready(function() {
+  var chosenMovie = [];
+
   for (i = 0; i < moviesArray.length; i++) {
     var thisMovie = "";
     thisMovie += "<ul>";
@@ -31,20 +35,23 @@ $(document).ready(function() {
 
   $("li").click(function() {
     //time
-    // alert(this.textContent);
-    // alert(this.parent().children(":first-child"));
-    console.log($(this).parent().children(":first-child").text());
-    var title = ($(this).prev("h3"));
-    // console.log(title.textContent)
-    // debugger;
+    chosenMovie.push(this.textContent);
+    // this.parent().children(":first-child");
+    //movie-title
+    chosenMovie.push($(this).parent().children(":first-child").text());
+
     $(".movieListing").hide();
     $(".agePrompt").show();
     $(".agePrompt").append("");
   });
 
-  // totalMovie.forEach(function(movie) {
-  //     $(".movieListing").append("<h3>" + movie.movieTitle +"</h3>"+ "<li>" +  movie.time + "</li>");
-  // });
+  $("form").submit(function(event) {
+    event.preventDefault();
+    chosenMovie.push($("#ageSelect").val());
+
+  });
+
+
 
 
 });
