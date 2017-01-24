6f882d3772d5ea4bf1e62cc1969ac58a04a18d18
Caitlin Hines and Elise St Hilaire <caitlin+elise@>
2017-01-24 11:55:58 -0800

    add for loop within for loop to loop through movies and times for movies

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,6 @@ var lalaLand = new Movie("La La Land", ["10am", "12pm", "3pm", "6pm", "10pm"]);
 var lionKing = new Movie("Lion King", ["10am", "12pm", "3pm", "6pm", "10pm"]);
 var princessBride = new Movie("Princess Bride", ["10am", "12pm", "3pm", "6pm", "10pm"]);
 
-var totalMovie = [lalaLand, lionKing, princessBride];
 
 
 
@@ -19,12 +18,20 @@ var totalMovie = [lalaLand, lionKing, princessBride];
 
 //front-end logic
 $(document).ready(function() {
-  $(".movieTimes").append(lalaLand);
-  $("form").submit(function(event) {
-    event.preventDefault();
+  for (i = 0; i < moviesArray.length; i++) {
+    $(".movieListing").append("<h3>" + moviesArray[i].movieTitle +"</h3>");
+    for (show = 0; show < moviesArray[i].time.length; show++) {
+      $(".movieListing").append("<li>" +  moviesArray[i].time[show] + "</li>");
+    }
+  }
 
+  $("li").click(function() {
+    $(this).addClass("red");
   });
-  totalMovie.forEach(function(movie) {
-      $(".movieListing").append("<h3>" + movie.movieTitle +"</h3>"+ "<li>" +  movie.time + "</li>");
-  });
+
+  // totalMovie.forEach(function(movie) {
+  //     $(".movieListing").append("<h3>" + movie.movieTitle +"</h3>"+ "<li>" +  movie.time + "</li>");
+  // });
+
+
 });
